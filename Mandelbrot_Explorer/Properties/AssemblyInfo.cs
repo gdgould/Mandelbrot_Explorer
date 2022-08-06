00f2bcceb9ed6355d36a1f42d4a4239c67beb1f3
gdgould <greyson.gould@protonmail.com>
2022-08-06 14:40:55 -0700

    Update version number to 0.8.1.3

--- Mandelbrot_Explorer/Properties/AssemblyInfo.cs
+++ Mandelbrot_Explorer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mandelbrot_Explorer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple program for rendering and navigating the Mandelbrot Set.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Greyson Gould")]
 [assembly: AssemblyProduct("Mandelbrot_Explorer")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.1.3")]
+[assembly: AssemblyFileVersion("0.8.1.3")]
